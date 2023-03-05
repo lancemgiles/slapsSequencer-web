289d954fc3ae2604070951877cb9b14536fd3b5e
greze <lmgiles6@gmail.com>
2023-03-05 15:50:08 -0500

    add volume control

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const audioCont = new (window.AudioContext || window.webkitAudioContext)();
+console.clear();
 
+//setup
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
+
+//volume controls
+const gainNode = audioCtx.createGain();
+const volumeControl = document.querySelector('[data-action="volume"]');
+volumeControl.addEventListener('input', function() {
+	gainNode.gain.value = this.value;
+}, false);
 
 // A minor pentatonic scale
 const A3 = 220;
@@ -9,20 +19,35 @@ const E4 = 329.63;
 const G4 = 392;
 const A4 = 440;
 
-const oscillator1 = audioCont.createOscillator();
-const oscillator2 = audioCont.createOscillator();
-const oscillator3 = audioCont.createOscillator();
-const oscillator4 = audioCont.createOscillator();
-const oscillator5 = audioCont.createOscillator();
-const oscillator6 = audioCont.createOscillator();
+
+const osc1 = audioCont.createOscillator();
+const osc2 = audioCont.createOscillator();
+const osc3 = audioCont.createOscillator();
+const osc4 = audioCont.createOscillator();
+const osc5 = audioCont.createOscillator();
+const osc6 = audioCont.createOscillator();
+
+osc1.type = osc2.type = osc3.type = osc4.type = osc5.type = osc6.type = 'square';
+
+osc1.frequency.setValueAtTime(A3, audioCont.currentTime);
+osc2.frequency.setValueAtTime(C4, audioCont.currentTime);
+osc3.frequency.setValueAtTime(D4, audioCont.currentTime);
+osc4.frequency.setValueAtTime(E4, audioCont.currentTime);
+osc5.frequency.setValueAtTime(G4, audioCont.currentTime);
+osc6.frequency.setValueAtTime(A4, audioCont.currentTime);
 
 
-oscillator1.type = oscillator2.type = oscillator3.type = oscillator4.type = oscillator5.type = oscillator6.type = 'triangle';
+osc1.connect(audioCont.destination);
+osc2.connect(audioCont.destination);
+osc3.connect(audioCont.destination);
+osc4.connect(audioCont.destination);
+osc5.connect(audioCont.destination);
+osc6.connect(audioCont.destination);
 
-
-oscillator1.frequency.setValueAtTime(A3, audioCont.currentTime);
-oscillator2.frequency.setValueAtTime(C4, audioCont.currentTime);
-oscillator3.frequency.setValueAtTime(D4, audioCont.currentTime);
-oscillator4.frequency.setValueAtTime(E4, audioCont.currentTime);
-oscillator5.frequency.setValueAtTime(G4, audioCont.currentTime);
-oscillator6.frequency.setValueAtTime(A4, audioCont.currentTime);
+//osc6.start();
+//document.getElementById("osc1").onclick = console.log("Test");
+const osc1_button = document.getElementById("osc1");
+if (osc1_button.addEventListener)
+    osc1_button.addEventListener("click", osc1.start(), false);
+else if (osc1_button.attachEvent)
+    osc1_button.attachEvent('onclick', osc1.start();
